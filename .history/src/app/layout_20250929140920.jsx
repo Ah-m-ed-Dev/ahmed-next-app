7483be3cb30f94439205cd7164e05e04115febfd
Ahmed <ahgadoora2002@gamil.com>
2025-09-29 14:12:16 +0200

    speed insights

--- .history/src/app/layout_20250929140920.jsx
+++ .history/src/app/layout_20250929140920.jsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import * as next from "@vercel/speed-insights/next"
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
