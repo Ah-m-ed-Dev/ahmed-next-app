f3a99aed43aa801a94729a989b2ccb7d9e460ac0
Ahmed <ahgadoora2002@gamil.com>
2025-09-30 14:16:04 +0200

    last

---
+++ .history/src/app/(home)/products/products_20250930141314.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Link from 'next/link';
+
+
+// const arr = [
+//   { productImg: "./images/1.png" },
+//   { productImg: "./images/2.webp" },
+//   { productImg: "./images/3.webp" },
+//   { productImg: "./images/4.webp" },
+//   { productImg: "./images/5.webp" },
+//   { productImg: "./images/6.webp" },
+//   { productImg: "./images/7.webp" },
+//   { productImg: "./images/8.png" },
+// ];
+
+async function getData() {
+  const res = await fetch("http://localhost:4000/products", {
+    next: { revalidate: 0 },
+  });
+
+  if (!res.ok) {
+    throw new Error("failed to fetch data");
+  }
+  return res.json();
+}
+
+const Products = async () => {
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const arrData = await getData();
+  
+
+  return (
+    <section className="products flex">
+      {arrData.map((item) => {
+        return (
+          <article title={item.title} key={item.id} className="card">
+            <Link href={`/product-details/${item.id}` }>
+              <img width={200} height={200} src={item.productImg} alt="" />
+            </Link>
+            <div style={{ width: "266px" }} className="content">
+              <h1 className="title">{item.title.slice(0, 10)}...</h1>
+              <p className="description">{item.description.slice(0, 100)}</p>
+              <div
+                className="flex"
+                style={{
+                  justifyContent: "space-between",
+                  paddingBottom: "0.7rem",
+                }}
+              >
+                <div className="price">${item.price}</div>
+                <button className="add-to-cart flex">
+                  <i className="fa-solid fa-cart-plus" />
+                  Add To Cart
+                </button>
+              </div>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Products;
---
+++ .history/src/app/product-details/[id]/page_20250930140256.jsx
@@ -0,0 +1,61 @@
+import Footer from "@/components/footer/footer"; 
+
+import Header from "@/components/header/header";
+import "./product-details.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+
+async function getData(iddd) {
+  // مثال باستخدام API حقيقية
+const res = await fetch("https://fakestoreapi.com/products");
+
+
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Page = async ({ params }) => {
+  const objData = await getData(params.id);
+  console.log(objData);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "grid",
+        alignItems: "center",
+        gridTemplateRows: "auto 1fr auto",
+      }}
+    >
+      <Header />
+
+      <main style={{ textAlign: "center" }} className="flex">
+       
+        <img alt="" src={`.${objData.productImg}`} />
+        <div className="product-details">
+          <div style={{ justifyContent: "space-between" }} className="flex">
+            <h2>{objData.title}</h2>
+            <p className="price">${objData.price}</p>
+          </div>
+          <p className="description">
+          {objData.description}
+          </p>
+          <button className="flex add-to-cart">
+           
+            <FontAwesomeIcon style={{width: "1.1rem"}} icon={faCartPlus}  />
+            Add To Cart
+          </button>
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Page;
